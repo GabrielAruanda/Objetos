359ed2dbc22bfb7482bb4e5be5aa578a88832933
GabrielAruanda <garuanda@gmail.com>
2024-01-09 15:06:02 -0800

    first commit

---
+++ java.js
@@ -0,0 +1,33 @@
+// let user = {
+//     nome: "joão",
+//     idade: 30
+    
+// }
+// alert(user.nome)
+// alert(user.idade)
+// user.futebol = confirm(`o ${user.nome} gosta de futebol?`)
+// alert(user.futebol)
+// alert (user[futebol])
+
+let user = {
+    // nome: "Gabriel Aruanda",
+    // idade: 30,
+    // salario: 1500,
+}
+   
+   user.nome = prompt ("Nome")
+   alert (user.nome)
+   
+   user.idade = prompt ("Idade")
+   alert (user.idade)
+   
+   user.salario = prompt ("Salario")
+   alert (user.salario)
+   
+//    while(nome == senha){ 
+//     alert ("Senha invalida")
+
+
+//     senha = prompt ("Insira a sua senha:")
+   
+//    senha = prompt ("Insira novamente a senha!")}
